e2058cb43d26b5759b210269a447900578c95bb2
merenefegil <efegileren@gmail.com>
2024-07-02 23:43:00 +0300

    Add photo, title and text

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import streamlit as st
+
+# st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2, gap="small")
+
+with col1:
+    st.image("imgs/8.png")
+
+with col2:
+    st.subheader("Mehmet Eren Efegil")
+    info_content = """
+    Learn Python from zero to complete by building real programs to gain the skills needed to land an entry-level job.
+    """
+    st.info(info_content)
